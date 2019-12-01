9f8276fa4fd2c62892bd8483f38f6355185326d0
magdanat <nathanmagdalera@gmail.com>
2019-12-01 04:10:53 -0800

    Work on games.js

--- servers/userservices/models/games/games.js
+++ servers/userservices/models/games/games.js
@@ -23,16 +23,28 @@ app.get("/", (req, res, next) => {
 // 401: Cannot verify players id_value
 // 500: Internal server error
 app.post("/", (req, res, next) => {
-    
+    if (!checkXUserHeader(req)) {
+        res.status(401).send("Unauthorized");
+    } else {
+        let user = JSON.parse(req.get('X-User'));
+        let description = req.body.description ? req.body.description : "";
+        // sql request to insert 
+    }
 });
 
 // Patch request to '/v1/game'
 // Update current game information including score and messages.
 // 201: application/json. Successfully updates a game
-// 401: player attempts to update game information not relating to them. Such as drawer, drawing_board, messages, or other people’s game_player
+// 401: player attempts to update game information not relating to them. 
+//      Such as drawer, drawing_board, messages, or other people’s game_player
 // 500: Internal server error
 app.patch("/", (req, res, next) => {
-    
+    if (!checkXUserHeader(req)) {
+        res.status(401).send("Unauthorized");
+    } else {
+        let user = JSON.parse(req.get('X-User'));
+
+    }
 });
 
 // Delete request to '/v1/game'
@@ -43,3 +55,19 @@ app.patch("/", (req, res, next) => {
 app.delete("/", (req, res, next) => {
 
 });
+
+
+// Helper Methods
+
+// Function to see if X-User header is present in request
+function checkXUserHeader(req) {
+    let xUserHeader = req.get('X-User');
+    if(xUserHeader == undefined || xUserHeader == "") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  // Need to export the router
+  module.exports = app;
