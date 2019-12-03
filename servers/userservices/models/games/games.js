08c853f50837827c519dfa77fbfcc695b7463193
Chad Ohta <chadohta@uw.edu>
2019-12-03 02:16:37 -0800

    work on apis

--- servers/userservices/models/games/games.js
+++ servers/userservices/models/games/games.js
@@ -23,6 +23,13 @@ const sqlDELETEGameByGameID = "DELETE FROM games WHERE GameID = ?";
 const sqlPOSTUserGames = "INSERT INTO users_game (GameID, UserID) VALUES (?, ?)"
 const sqlDELETEGamePlayerByID = "DELETE FROM users_game WHERE UserID = ? AND GameID = ?";
 
+const sqlGETGameInstanceByID = "SELECT * FROM Games_Instance WHERE GameInstanceID = ?";
+const sqlPOSTGameInstance = "INSERT INTO Games_Instance (GameID, NumberOfRounds, BoardID) VALUES(?, ?, ?)"
+const sqlPATCHGameInstance = "UPDATE Games_Instance SET CurrentDrawer = ?, CurrentRound = ?, CurrentWord = ?, Winner = ?, Score = ?, WHERE GameInstancID = ?";
+const sqlPOSTBoard = "INSERT INTO Board (Drawing) VALUES(?)"
+const sqlPATCHBoardByID = "UPDATE Board SET Drawing = ?, WHERE BoardID = ?";
+
+
 // Connection to the mysql database
 let connection = mysql.createPool({
     // We are going to need to set this ENV variable, TODO
@@ -298,7 +305,22 @@ app.post(":gameID/instance", (req, res, next) => {
     if (!checkXUserHeader(req)) {
         res.status(401).send("Unauthorized");
     } else {
-        // query to insert into Games_Instance Table
+        // create a board, get the board ID in return
+        let newBoard = ""; // empty string = blank board
+        connection.query(sqlPOSTBoard, [newBoard], (err, result) => {
+            let boardID = result.boardID;
+            // query to insert into Games_Instance Table
+            connection.query(sqlPOSTGameInstance, [req.params.gameID, req.body.numRounds, boardID], (err, result) => {
+                if (err) {
+                    res.status(500).send("Internal Server Error");
+                } else {
+                    res.status(201);
+                    res.set("Content-Type", "application/json");
+                    res.json(result);
+                }
+            })
+
+        })
     }
 })
 
@@ -308,11 +330,34 @@ app.post(":gameID/instance", (req, res, next) => {
 // Mainly going to be used to update drawing board information.
 // 201: application/json. Successfully makes changes to the game instance.
 // 500: Internal server error.
-app.patch(":gameID/instance", (req, res, next) => {
+app.patch(":gameID/instanceID", (req, res, next) => {
     if (!checkXUserHeader(req)) {
         res.status(401).send("Unauthorized");
     } else {
-        // query to make changes in Games_Instance Table
+        // query current game instance and get all data
+        connection.query(sqlGETGameInstanceBYID, [req.params.instanceID], (err, result) => {
+            if (err) {
+                res.status(500).send("Internal Server Error");
+            } else {
+                // check each variable, if missing insert current data
+                let currDrawer = req.body.currentDrawer ? req.body.currentDrawer : result.currentDrawer;
+                let currRound = req.body.currentRound ? req.body.currentRound : result.currentRound;
+                let currWord = req.body.currentWord ? req.body.currentWord : result.currentWord;
+                let winner = req.body.winner? req.body.winner : result.winner;
+                let score = req.body.score ? req.body.score : result.score;
+
+                // query to make changes in Games_Instance Table
+                connection.query(sqlPATCHGameInstance, [currDrawer, currRound, currWord, winner, score, req.params.instanceID], (err, result) => {
+                    if (err) {
+                        res.status(500).send("Internal Server Error");
+                    } else {
+                        res.status(201);
+                        res.set("Content-Type", "application/json");
+                        res.json(result);
+                    }
+                })
+            }
+        })
     }
 })
 
@@ -320,29 +365,48 @@ app.patch(":gameID/instance", (req, res, next) => {
 // /v1/game/:gameID/instance/board //
 /////////////////////////////////////
 
-// Post 
-// Post request to add coordinates to the drawing board.
-// If the user is not the current drawer, do not allow them
-// to draw on the board or make this request. 
-app.post(":gameID/instance/board"), (req, res, next) => {
+// PATCH
+// Updates Board, can update with empty string (blank board) to drawer changes
+// I imagine this will be used when the board is reset
+// every turn or once the current drawer changes. 
+app.patch(":gameID/instance/board", (req, res, next) => {
     if (!checkXUserHeader(req)) {
         res.status(401).send("Unauthorized");
     } else {
-        // query to insert into Coordinates table 
+        // 
+        connection.query(sqlGETGameInstanceByID, [req.params.instanceID], (err, result) => {
+            if (err) {
+                res.status(500).send("Internal Server Error");
+            } else {
+                let boardID = result.boardID;
+                connection.query(sqlPATCHBoardByID, [req.drawing, boardID], (err, result) => {
+                    if (err) {
+                        res.status(500).send("Internal Server Error");
+                    } else {
+                        res.status(201);
+                        res.set("Content-Type", "application/json");
+                        res.json(result);
+                    }
+                })
+            }
+        })
+
     }
 })
 
-// Delete
-// Deletes all coordinate information.
-// I imagine this will be used when the board is reset
-// every turn or once the current drawer changes. 
-app.delete(":gameID/instance/board"), (req, res, next) => {
-    if (!checkXUserHeader(req)) {
-        res.status(401).send("Unauthorized");
-    } else {
-        // query ty delete coordinates table
-    }
-})
+
+///////////////////////
+// Messages (Answer) //
+///////////////////////
+
+// Post
+// 
+
+
+// Patch 
+// 
+
+
 
 
 ////////////////////
@@ -373,3 +437,5 @@ function checkIfCreator(req, result) {
   
   // Need to export the router
   module.exports = app;
+
+// patch request to game instance, get new word
